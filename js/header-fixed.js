be547fe74bb4de19255010e30c9fef2848d8eb22
CelWeb <reder121@gmail.com>
2018-11-06 00:03:31 +0300

    movie__premiere

---
+++ js/header-fixed.js
@@ -0,0 +1,8 @@
+         window.onscroll = function showHeader() {
+    var header = document.querySelector('.header');
+    if(window.pageYOffset > 10){
+        header.classList.add('header__fixed');
+    } else{
+        header.classList.remove('header__fixed');
+    }
+}
